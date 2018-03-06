268046824ce65a47741dfb812914898a1013df56
xuedong <shang.xuedong@yahoo.fr>
2018-03-07 00:23:13 +0100

    Start from scratch.

--- SuperSpaceMan/SuperSpaceMan/GameViewController.swift
+++ SuperSpaceMan/SuperSpaceMan/GameViewController.swift
@@ -6,50 +6,28 @@
 //  Copyright © 2018 xuedong. All rights reserved.
 //
 
-import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    var scene: GameScene!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
+        // Configure the main view
+        let skView = view as! SKView
+        skView.showsFPS = true
+        
+        // Create and configure the game scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        // Show the scene
+        skView.presentScene(scene)
     }
 }
