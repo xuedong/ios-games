0b70f2a74ba95b6d19a54d2d8207cc56e6caab42
xuedong <shang.xuedong@yahoo.fr>
2018-03-30 00:49:22 +0200

    Added the first background and player.

--- SuperSpaceMan/SuperSpaceMan/GameScene.swift
+++ SuperSpaceMan/SuperSpaceMan/GameScene.swift
@@ -10,6 +10,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var backgroundNode = SKSpriteNode(imageNamed: "Background")
+    let playerNode = SKSpriteNode(imageNamed: "Player")
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -17,5 +20,14 @@ class GameScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
         backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        // adding the background
+        backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        backgroundNode.position = CGPoint(x: size.width / 2.0, y: 0.0)
+        addChild(backgroundNode)
+        
+        // adding the player
+        playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        addChild(playerNode)
     }
 }
