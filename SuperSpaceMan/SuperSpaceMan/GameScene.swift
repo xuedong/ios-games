bdd4dc5f26285dd3f674c8a040e4f8f02c7f0be7
xuedong <shang.xuedong@yahoo.fr>
2018-04-26 00:54:27 +0200

    Added first physics action.

--- SuperSpaceMan/SuperSpaceMan/GameScene.swift
+++ SuperSpaceMan/SuperSpaceMan/GameScene.swift
@@ -19,7 +19,11 @@ class GameScene: SKScene {
     
     override init(size: CGSize) {
         super.init(size: size)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
         backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        // user interaction
+        isUserInteractionEnabled = true
         
         // adding the background
         backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0)
@@ -28,6 +32,14 @@ class GameScene: SKScene {
         
         // adding the player
         playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        playerNode.physicsBody?.linearDamping = 1.0
+        playerNode.physicsBody =
+            SKPhysicsBody(circleOfRadius: playerNode.size.width / 2.0)
+        playerNode.physicsBody?.isDynamic = true
         addChild(playerNode)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        playerNode.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
     }
 }
